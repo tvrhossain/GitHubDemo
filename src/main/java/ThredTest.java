ba0f856b8664a7ce14b24b8ec4a4e21aeb49057f
tanvirtonu <mailhossain2013@gmail.com>
2020-09-14 00:53:47 +0600

    New Runnable Class Added

--- src/main/java/ThredTest.java
+++ src/main/java/ThredTest.java
@@ -3,14 +3,15 @@ public class ThredTest {
     public static void main(String[] args) {
         System.out.println("");
 
-        Runnable task = () -> {
-            System.out.println("THIS IS FIRST THREAD- HELLO WORLD WAR 3");
-        };
+        Runnable task = () -> System.out.println("THIS IS FIRST THREAD- HELLO WORLD WAR 3");
 
 
         Thread me = new Thread( task);
         me.start();
         me.interrupt();
+
+        Thread me2= new Thread( new NewTask());
+        me2.start();
     }
 }
 
@@ -19,6 +20,6 @@ class NewTask implements Runnable{
 
     @Override
     public void run() {
-
+        System.out.println("Optimum way of making thread");
     }
 }
